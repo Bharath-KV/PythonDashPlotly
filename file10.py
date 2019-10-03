e269bd6639c47f656d47556c456bab07b17dbb12
Bharath-KV <bharathkv48@gmail.com>
2019-10-03 22:33:52 +0530

    Dash State

---
+++ file10.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Input(id='input-state-one', type='text', value='Bharath'),
+    dcc.Input(id='input-state-two', type='text', value='Sravani'),
+    html.Button(id='submit-button', n_clicks=0, children='SUBMIT'),
+    html.Div(id='output-state')
+])
+
+
+@app.callback(
+    Output('output-state', 'children'),
+    [Input('submit-button', 'n_clicks')],
+    [State('input-state-one', 'value'),
+     State('input-state-two', 'value')]
+)
+def update_state(n_clicks, state_one, state_two):
+    return '''The Submit Button has been pressed {} times.\n
+              {} Loves {}.
+    '''.format(n_clicks, state_one, state_two)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=3000)
